857e0f504ca530e3d72af6dc4ba71a69f69e076a
Michael Hayes <michael.hayes@urbanairship.com>
2014-09-09 21:27:04 -0700

    more css

--- client.js
+++ client.js
@@ -11,6 +11,8 @@ altr.addFilter('remaining', function(change) {
 var main_el = document.querySelector('[rel=main]')
 var arrival_el = document.querySelector('[rel=arrivals]')
 var detail_el = document.querySelector('[rel=details]')
+var sidebar_toggle = document.querySelector('[rel=sidebar-toggle]')
+var sidebar_el = document.querySelector('[rel=sidebar]')
 var view = altr(main_el, {})
 
 arrival_el.removeAttribute('style')
@@ -23,6 +25,8 @@ detail_el.addEventListener('click', function() {
   arrival_el.classList.remove('off-left')
   detail_el.classList.add('off-right')
 })
+
+sidebar_toggle.addEventListener('click', toggle_sidebar)
 
 var timer
 
@@ -40,4 +44,9 @@ function update(location) {
     view.update({lines: JSON.parse(data), location: location}, true)
     timer = setTimeout(update.bind(null, location), 5000)
   }))
+}
+
+function toggle_sidebar(ev) {
+  ev.preventDefault()
+  sidebar_el.classList.toggle('hidden')
 }
