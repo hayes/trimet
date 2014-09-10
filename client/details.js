3daf7aba30a701278f4da15baeb48c72371cfe4b
Michael Hayes <michael.hayes@urbanairship.com>
2014-09-10 10:19:56 -0700

    details

---
+++ client/details.js
@@ -0,0 +1,29 @@
+var altr = require('altr')
+var fs = require('fs')
+
+module.exports = Details
+
+var template = fs.readFileSync(
+    __dirname + '/templates/line.html'
+  , 'utf8'
+)
+
+function Details(arrivals, i) {
+  if(!(this instanceof Details)) {
+    return new Details(arrivals, i)
+  }
+
+  this.data = {}
+  this.title = 'Details'
+  this.waiting = true
+  this.view = altr(template, this)
+  this.el = this.view.rootNodes()
+  arrivals.view.lookups.register('lines.' + i, this.update.bind(this))
+}
+
+Details.prototype.update = update
+
+function update(data) {
+  this.data = JSON.stringify(data, null, 2)
+  this.view.update(this)
+}
