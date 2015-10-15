4751cc3fcd093dd1e93a17a8e9a120a99cdbc445
Michael Hayes <mhayes@newrelic.com>
2015-10-15 10:07:28 -0700

    change license key

--- newrelic.js
+++ newrelic.js
@@ -8,17 +8,17 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name : ['hayes-trimet-test'],
+  app_name : ['trimet-test'],
   /**
    * Your New Relic license key.
    */
-  license_key : '43c41673c6c0e1893649e9f98fed60be8435579f',
+  license_key : '744682ade4f6cb0f8e8c1a709d3174f904dc9d2b',
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : 'trace'
+    level : 'info'
   }
 }
